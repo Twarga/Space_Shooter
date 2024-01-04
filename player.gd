f4c4c926ef55a81e1e96ca353f0db55315272145
Youness Touzani <103898544+Twarga@users.noreply.github.com>
2024-01-04 21:59:17 +0100

    shooting

--- player.gd
+++ player.gd
@@ -23,7 +23,29 @@ func _process(delta):
 		$Ship/Boosters.animation = "forward"
 	position += input * speed * delta
 	position = position.clamp(Vector2(8, 8), screensize-Vector2(8, 8))
-	
+	if Input.is_action_pressed("shoot"):
+		shoot()
+
+
 func start():
 	position = Vector2(screensize.x / 2 , screensize.y - 64)
 	$GunCooldown.wait_time - cooldown
+
+
+	
+
+
+func shoot():
+	if not can_shoot:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position + Vector2(0, -8 ))
+
+
+
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
