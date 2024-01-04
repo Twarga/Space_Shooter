070725e926464231d6f00e34f4d44aea76943159
Youness Touzani <103898544+Twarga@users.noreply.github.com>
2024-01-04 21:31:04 +0100

    bullet + player 1

--- bullet.gd
+++ bullet.gd
@@ -11,3 +11,13 @@ func _process(delta):
 	
 	
 	
+
+
+func _on_area_entered(area):
+	if area.is_in_group("enemies"):
+		area.explode()
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- player.gd
+++ player.gd
@@ -1,7 +1,15 @@
 extends Area2D
-
+# vars 
 @export var speed = 150
 @onready var screensize = get_viewport_rect().size
+@export var cooldown = 0.25 
+@export var bullet_scene : PackedScene
+var can_shoot = true
+
+func _ready():
+	start()
+	
+
 func _process(delta):
 	var input = Input.get_vector("left", "right", "up", "down")
 	if input.x > 0:
@@ -15,3 +23,7 @@ func _process(delta):
 		$Ship/Boosters.animation = "forward"
 	position += input * speed * delta
 	position = position.clamp(Vector2(8, 8), screensize-Vector2(8, 8))
+	
+func start():
+	position = Vector2(screensize.x / 2 , screensize.y - 64)
+	$GunCooldown.wait_time - cooldown
