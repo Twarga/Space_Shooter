7eca34e83c3124f8a0d77c8c60a77a6e2c24afc8
Youness Touzani <103898544+Twarga@users.noreply.github.com>
2024-01-05 16:08:06 +0100

    enemy bug kinda fixed

--- enemy.gd
+++ enemy.gd
@@ -7,32 +7,30 @@ var speed = 0
 @onready var screensize = get_viewport_rect().size
 
 func start(pos):
-	speed = 0 
-	position = Vector2(pos.x , -pos.y)
+	speed = 0
+	position = Vector2(pos.x, -pos.y)
 	start_pos = pos
 	await get_tree().create_timer(randf_range(0.25, 0.55)).timeout
 	var tween = create_tween().set_trans(Tween.TRANS_BACK)
-	tween.tween_property(self, "position:y" , start_pos.y , 1.4)
+	tween.tween_property(self, "position:y", start_pos.y, 1.4)
 	await tween.finished
 	$MoveTimer.wait_time = randf_range(5, 20)
 	$MoveTimer.start()
 	$ShootTimer.wait_time = randf_range(4, 20)
 	$ShootTimer.start()
 	
-	
 func _process(delta):
 	position.y += speed * delta 
 	if position.y > screensize.y + 32 :
 		start(start_pos)
 
-func exploid():
+func explode():
 	speed = 0
-	$AnimationPlayer.play("exploid")
+	$AnimationPlayer.play("explode")
 	set_deferred("monitoring", false)
 	died.emit(5)
 	await $AnimationPlayer.animation_finished
 	queue_free()
-	
 	
 signal died
 	
--- main.gd
+++ main.gd
@@ -8,14 +8,17 @@ func _ready():
 	spawn_enemies()
 	
 	
+
+
 func spawn_enemies():
-	for i in range(9):
+	for x in range(9):
 		for y in range(3):
 			var e = enemy.instantiate()
-			var pos = Vector2(x * (16 + 8) + 24, 16 * 4 + y * 16 )
+			var pos = Vector2(x * (16 + 8) + 24, 16 * 4 + y * 16)
 			add_child(e)
 			e.start(pos)
 			e.died.connect(_on_enemy_died)
+			
 
 func _on_enemy_died(value):
 	score += value
